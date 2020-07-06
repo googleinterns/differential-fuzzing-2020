847821f8e6a9a1f9fb5264d84da563718a77e5c0
pgmoka <pgmoka@live.com>
2020-07-06 16:03:13 +0000

    Better code standards, naming conventions, and documentation

--- differentialfuzzer.cpp
+++ differentialfuzzer.cpp
@@ -7,12 +7,16 @@
 
 namespace differential_fuzzer
 {
+    // Linked list used to save the names associated with parsers
+    // for EquivalenceParserOutputs
     struct AssociatedParserName
     {
-        std::string data;
+        std::string name;
         AssociatedParserName* next_container;
     };
 
+    // Linked list used to transverse between previous sets of parsers.
+    // One representative for each set is saved for future comparisons.
     struct EquivalenceParserOutputs
     {
         EquivalenceParserOutputs* next;
@@ -28,7 +32,7 @@ namespace differential_fuzzer
     {    
         AssociatedParserName* prev = delete_me;
 
-        while (delete_me != NULL)
+        while (delete_me != nullptr)
         {
             
             delete_me = delete_me->next_container;
@@ -48,7 +52,7 @@ namespace differential_fuzzer
     {
         EquivalenceParserOutputs* prev = delete_me;
         
-        while (delete_me != NULL)
+        while (delete_me != nullptr)
         {
             delete_me = delete_me->next;
 
@@ -67,21 +71,21 @@ namespace differential_fuzzer
     // --------------------------- struc print functions -------------------------------
     // ---------------------------------------------------------------------------------
 
-    void PrintParserContainers(AssociatedParserName* head)
+    void PrintAssociatedParserName(AssociatedParserName* head)
     {
         AssociatedParserName* ptr = head;
 
-        if (ptr!=NULL)
+        if (ptr!=nullptr)
         {
             std::cerr << "- ";
         }
 
-        while (ptr!=NULL)
+        while (ptr!=nullptr)
         {
-            std::cerr << ptr->data;
+            std::cerr << ptr->name;
             ptr = ptr->next_container;
 
-            if (ptr==NULL)
+            if (ptr==nullptr)
             {
                 std::cerr << std::endl;
             }
@@ -92,8 +96,15 @@ namespace differential_fuzzer
         }
     }
 
-
-    void PrintNodes(EquivalenceParserOutputs* head)
+    // Ouput in cerr:
+    // Ex.:
+    //--- Printing Results
+    // Set #1:
+    // - parser_2
+    // Set #2:
+    // - parser_1
+    // - parser_3
+    void PrintEquivalenceParserOutputs(EquivalenceParserOutputs* head)
     {
         EquivalenceParserOutputs* ptr = head;
 
@@ -101,15 +112,15 @@ namespace differential_fuzzer
 
         int set_id = 1;
 
-        while (ptr!=NULL)
+        while (ptr!=nullptr)
         {
             std::cerr << "Set #" << set_id << ":" << std::endl;
             
             set_id++;
 
-            if (ptr->container_basket!=NULL)
+            if (ptr->container_basket!=nullptr)
             {
-                PrintParserContainers(ptr->container_basket);
+                PrintAssociatedParserName(ptr->container_basket);
             }
             else
             {
@@ -125,30 +136,43 @@ namespace differential_fuzzer
     // ------------------------------ struc functions ----------------------------------
     // ---------------------------------------------------------------------------------
 
-    void AddDataToBasket(AssociatedParserName** head, std::string name)
+    void AddToAssociatedParserName(AssociatedParserName** head, std::string name)
     {
         AssociatedParserName* ptr =  new AssociatedParserName;
 
-        ptr->data = name;
+        ptr->name = name;
         ptr->next_container = *head;
 
         *head = ptr;
     }
 
+    // Function checks if a parser has an output that matches the representative of one 
+    // of the already existing EquivalenceParserOutputs list. If the output of the new
+    // parser matches a representative, its name is saved to the AssociatedParserName 
+    // list of that particular EquivalenceParserOutputs node. If the output of a new 
+    // parser does not match a previous representative, a new EquivalenceParserOutputs 
+    // node is created, and added to the original EquivalenceParserOutputs with the 
+    // output of the parser being saved as the representative of that 
+    // EquivalenceParserOutputs node.
+    //
+    // If the parser output has matched a previous representative, the function returns
+    // true. If the parser output has not matched a previous representative, the
+    // function returns false
     bool CheckAndAdd(differential_parser::Parser* parser, EquivalenceParserOutputs** head)
     {
         // Iterator:
         EquivalenceParserOutputs* ptr = *head;
 
-        void* parser_output = parser->normalize(NULL);
-
-        while (ptr != NULL)
+        void* parser_output = parser->normalize(nullptr);
+        
+        // Iterate through the different EquivalenceParserOutputs
+        while (ptr != nullptr)
         {
             if ( parser->equivalent(ptr->representative, parser_output))
             {
                 // add to parse basket
 
-                AddDataToBasket(&ptr->container_basket, parser->getName());
+                AddToAssociatedParserName(&ptr->container_basket, parser->getName());
                 delete((std::string *)(parser_output));     // (placeholder)
                 return true;
             }
@@ -158,8 +182,8 @@ namespace differential_fuzzer
         ptr = new EquivalenceParserOutputs;
         ptr->representative = parser_output;
         ptr->container_basket = new AssociatedParserName;
-        ptr->container_basket->data = parser->getName();
-        ptr->container_basket->next_container = NULL;
+        ptr->container_basket->name = parser->getName();
+        ptr->container_basket->next_container = nullptr;
         ptr->next = *head;
 
         *head = ptr;
@@ -172,8 +196,19 @@ namespace differential_fuzzer
     // ----------------------------- Fuzzing Methd -------------------------------------
     // ---------------------------------------------------------------------------------
 
-    void DifferentiallyFuzz(differential_parser::Parser** parser_array, int number_of_parsers)
+    // Given an array of pointers to parsers, the function will apply the process
+    // of differential fuzzing to the parsers given. Parsers are grouped in sets
+    // where each set has a corresponding output.
+    void DifferentiallyFuzz(differential_parser::Parser** parser_array, 
+        int number_of_parsers)
     {
+        if (number_of_parsers <= 0)
+        {
+            std::cerr << "ERROR: No parsers passed to 'DifferentiallyFuzz' function" 
+                << std::endl;
+            return;
+        }
+
         // Creates iteration structure
         EquivalenceParserOutputs *head = new EquivalenceParserOutputs;
         
@@ -181,11 +216,11 @@ namespace differential_fuzzer
         bool are_all_parsers_equal = true;
 
         // First case:
-        head->representative = parser_array[0]->normalize(NULL);
-        head->next = NULL;
+        head->representative = parser_array[0]->normalize(nullptr);
+        head->next = nullptr;
         head->container_basket = new AssociatedParserName;
-        head->container_basket->data = parser_array[0]->getName();
-        head->container_basket->next_container = NULL;
+        head->container_basket->name = parser_array[0]->getName();
+        head->container_basket->next_container = nullptr;
 
         for (int i=1; i<number_of_parsers;i++)
         {
