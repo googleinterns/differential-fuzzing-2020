dd70c0c1751d597a3ffc8e75d59905b261d39bf4
pgmoka <pgmoka@live.com>
2020-07-18 01:13:39 +0000

    Refactoring for Examples, and Start of Proto yaml-cpp Parsing

--- testmain.cpp
+++ testmain.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 // From git file
 #include "include/yaml.h"
@@ -15,7 +16,7 @@ struct thing
 };
 
 // Main used for the purpose of minor testing
-int main() 
+int main()
 {
     std::cout << "Do thing!" << std::endl;
     
@@ -24,9 +25,16 @@ int main()
     yaml_parser_t parser;
     yaml_event_t event;
     yaml_emitter_t emitter;
+
+    memset(&parser, 0, sizeof(parser));
+    memset(&event, 0, sizeof(event));
+    memset(&emitter, 0, sizeof(emitter));
     int done = 0;
 
-    FILE* output = fopen("strings.yaml","r");
+    // global-tag.yaml(yaml-cpp error)  mapping.yaml  strings.yaml
+    std::string path_to_test_file = "examples/strings.yaml";
+
+    FILE* output = fopen(path_to_test_file.c_str(),"r");
     FILE* output_for_emitter = fopen("tempyamloutput.txt","w");
     // FILE* output = fopen("tempyamloutput.txt","r");
 
@@ -45,14 +53,26 @@ int main()
     // size_t string_size = 2;
     // char start[2] = "o";
 
-    size_t string_size = 8;
-    char start[8] = "1e ]e \t";
+    // size_t string_size = 8;
+    // char start[8] = "1e ]e \t";
+    // const std::string temp = "folded: >\n    This entire block of text will be the value of 'folded', but this\n    time, all newlines will be replaced with a single space";
+    // const std::string temp = "base: &base\n name: Everyone has same name\n\n foo: &foo\n <<: *base\n age: 10";
+
+    // size_t string_size = 140;
+    // char start[140] = "folded: >\n    This entire block of text will be the value of 'folded', but this\n    time, all newlines will be replaced with a single space";
+
+    size_t string_size = 74;
+    char start[74] = "base: &base\n name: Everyone has same name\n\n foo: &foo\n <<: *base\n age: 10";
+    
+    // size_t string_size = 1000;
+    // char start[22] = "{\"key\": [\"value\", 3]}";
+    const std::string temp = std::string(start, string_size-1);
 
     // size_t string_size = strlen(start);
     
     // size_t string_size = 300;
 
-    unsigned char* buffer = new unsigned char[100];
+    unsigned char* buffer = new unsigned char[1000];
 
     size_t written = 0;
 
@@ -60,22 +80,22 @@ int main()
     {
         return 0;
     }
-
-    yaml_parser_set_input_string(&parser, (yaml_char_t*)start, string_size - 1);
-    // yaml_parser_set_input_file(&parser,output);
     
     if(!yaml_emitter_initialize(&emitter))
     {
         return 0;
     }
-
     
-    // yaml_emitter_set_output_file(&emitter,output_for_emitter);
-    yaml_emitter_set_output_string(&emitter, buffer, string_size-1, &written);
+    yaml_parser_set_input_string(&parser, (yaml_char_t*)start, string_size - 1);
+    // yaml_parser_set_input_file(&parser,output);
+    
+    // yaml_emitter_set_output_file(&emitter,output_for_emitter); //output_for_emitter
+    // yaml_emitter_set_output_file(&emitter,stdout); //output_for_emitter
+    yaml_emitter_set_output_string(&emitter, buffer, string_size + 2, &written);
 
     yaml_emitter_set_unicode(&emitter, 1);
 
-    // yaml_emitter_set_canonical(&emitter, 1);
+    // yaml_emitter_set_canonical(kk&emitter, 1);
 
     while (!done)
     {
@@ -95,27 +115,43 @@ int main()
         {
             std::cout << "----BAD EMITTER----" << std::endl;
 
+            switch (emitter.error)
+            {
+                case YAML_MEMORY_ERROR:
+                    fprintf(stderr, "Memory error: Not enough memory for emitting\n");
+                    break;
+
+                case YAML_WRITER_ERROR:
+                    fprintf(stderr, "Writer error: %s\n", emitter.problem);
+                    break;
+
+                case YAML_EMITTER_ERROR:
+                    fprintf(stderr, "Emitter error: %s\n", emitter.problem);
+                    break;
+
+                default:
+                    /* Couldn't happen. */
+                    fprintf(stderr, "Internal error\n");
+                    break;
+            }
         }
-
         // std::cout << "- Output from emitter: "<< buffer << std::endl;
-
-        std::cout << "- Output from raw buffer parser: "<< (char*)parser.raw_buffer.start << std::endl;
-        std::cout << "- Output from emitter: "<< buffer << std::endl;
     }
 
-    std::cout << "- Print again not knowing size "<< buffer << std::endl;
+    std::cout << "- Print again not knowing size " << std::endl;
 
+    std::cout << buffer << std::endl;
     int pos_in_buffer = 0;
 
     int temp_interator_counter = string_size;
 
-    while(buffer[pos_in_buffer]!=NULL && temp_interator_counter>1)
-    {
-        std::cout << buffer[pos_in_buffer];
-        pos_in_buffer++;
-        temp_interator_counter--;
-    }
-    std::cout << std::endl;
+    // while(buffer[pos_in_buffer]!=NULL && temp_interator_counter>1)
+    // {
+    //     std::cout << buffer[pos_in_buffer];
+    //     pos_in_buffer++;
+    //     temp_interator_counter--;
+    // }
+    std::cout <<  "----" << std::endl;
 
     yaml_event_delete(&event); // may be leaking
 
@@ -128,29 +164,70 @@ int main()
     const uint8_t input_data[] = {'b', 'e'};
     int input_data_size = 2;
     const std::string hello((const char*)input_data, input_data_size);
+
+    std::string yamlcpp_final_output;
     try
     {   
-        YAML::Node primes = YAML::LoadFile("strings.yaml");
-        // YAML::Node primes = YAML::Load("["+ std::string(start) +"]");
-        // YAML::Node primes = YAML::Load("["+ hello +"]");
-        std::cout << primes["literal-block"] << std::endl;
+        // YAML::Node node = YAML::LoadFile(path_to_test_file);
+        YAML::Node node = YAML::Load("[2, 3]");
+        // YAML::Node node = YAML::Load(temp);
+        // YAML::Node node = YAML::Load("Hello World");
+        std::cout << "Node type: " << node.Type() << std::endl;
 
-        // for (YAML::const_iterator it=primes.begin();it!=primes.end();++it) 
-        // {
-        //     std::string yaml_cpp_loop_temp = it->as<std::string>();
-            
-        //     std::cout << yaml_cpp_loop_temp << std::endl;
-        //     std::cout << "libyaml == yaml-cpp: " << 
-        //         (strcmp((const char*)yaml_cpp_loop_temp.c_str(),(char*)(buffer)) == 0) << std::endl;
-        //     std::cout << "---" << std::endl;
+        // Makes selection of how to parse node
+        if (node.Type() == 1) // NULL
+        {
+            yamlcpp_final_output = "- Null case";
+        }
+        else if (node.Type() == 2) // Scalar
+        {
+            yamlcpp_final_output = node.Scalar();
+        }
+        else if (node.Type() == 3) // Sequence
+        {
+            for (std::size_t i=0;i<node.size();i++) 
+            {
+                yamlcpp_final_output += "- " + node[i].as<std::string>() +"\n";
+            }
+        }
+        else if (node.Type() == 4) // Dictionary
+        {
+            std::cout << "----- Before looping: " << std::endl;
 
-        //     // parser.raw_buffer.start==yaml_cpp_loop_temp
-        // }
+            for (YAML::const_iterator it=node.begin();it!=node.end();++it) 
+            {
+                std::cout << "- "<< it->first.as<std::string>() << std::endl;
+
+                std::cout << node[it->first.as<std::string>()].as<std::string>() << std::endl;
+            }
+            std::cout << "----- After looping" << std::endl;
+        }
+        else if (node.Type() == 5) // Map (only for special cases, so it may never be called)
+        {
+            std::cout << "I am a map!" << std::endl;
+        }
+        else // Unknown
+        {
+            std::cerr << "ERROR: Unknown Input Type" << std::endl;
+        }
+
+
+        // std::cout << yamlcpp_output.str() << std::endl;
+        
+        // YAML::Node node = YAML::Load("{theng: boeeee, thang: boaaaa, list: [3,2], listother: {list321: keep}, empty}");
+        // YAML::Node node = YAML::Load("start: [1, 3, 0], second: booeee");
+        
+        // YAML::Node node = YAML::Load("["+ std::string(start) +"]");
+        // YAML::Node node = YAML::Load("["+ hello +"]");
+
+        // std::cout << node["literal-block"] << std::endl;
     }
     catch(const std::exception& err)
     {
         std::cout << err.what() << std::endl;
     }
+
+    std::cout << "Final output: " << std::endl << yamlcpp_final_output << std::endl;
 
     // YAML::Emitter out;
     // out << std::string(start);
