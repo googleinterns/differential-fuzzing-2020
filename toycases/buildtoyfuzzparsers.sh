7994d57ad0ebec78647d8363883bc4b20fb66ea1
pgmoka <pgmoka@live.com>
2020-08-12 23:37:25 +0000

    Refactor toycases file

--- toycases/buildtoyfuzzparsers.sh
+++ toycases/buildtoyfuzzparsers.sh
@@ -1,4 +1,5 @@
 clang++ ../../../libraries/libyaml/src/.libs/libyaml.a ../../../libraries/yaml-cpp/build/libyaml-cpp.a \
--c toyfuzzintparser.cpp toyfuzzswitchparser.cpp toyfuzzstringoutputbase.cpp differentialfuzzer.o \
+-c ./code/toyfuzzintparser.cpp ./code/toyfuzzswitchparser.cpp ./code/toyfuzzstringoutputbase.cpp \
+./differentialfuzzer.o \
 -I ../ -I ./../../../libraries/yaml-cpp/include -I ./../../libraries/libyaml/include \
--I ../differentialfuzzinglib/
+-I ../differentialfuzzinglib/ -I ./code
--- toycases/runtoyfuzzer.sh
+++ toycases/runtoyfuzzer.sh
@@ -2,5 +2,6 @@ rm *.o testexecutable *.h.gch
 bash buildtoybase.sh 
 bash buildtoyfuzzparsers.sh
 
-clang++ differentialfuzzer.o toyfuzzstringoutputbase.o toyfuzzintparser.o toyfuzzswitchparser.o toytests.cpp \
+clang++ differentialfuzzer.o toyfuzzstringoutputbase.o toyfuzzintparser.o toyfuzzswitchparser.o \
+./code/toytests.cpp \
 -o toytestexecutable -I ../ -I ../differentialfuzzinglib/
