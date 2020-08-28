a6c4bc140a8dfe0eef3c479b46d16425360f390a
pgmoka <pgmoka@live.com>
2020-08-28 23:36:40 +0000

    Better naming conventions, and libyaml wrapper tests

---
+++ tests/libyaml_wrapper_tests/create_libyaml_parser_sanity_test.sh
@@ -0,0 +1,13 @@
+bash cleanup.sh 
+
+bash buildbase.sh 
+
+bash buildyamlfuzzparsers.sh
+
+source ./paths_and_variables.sh
+
+clang++ differential_fuzzer.o comparison_utils.o libyaml_utils.o libyaml_parser.o yamlcpp_parser.o ./code/test_libyaml_parser_fuzz.cpp \
+yaml_normalized_output.o -I "$YAML_LIBRARY_PATHS"yaml-cpp/include \
+-I ./../../differential_fuzzing_lib/ \
+"$YAML_LIBRARY_PATHS"libyaml/src/.libs/libyaml.a "$YAML_LIBRARY_PATHS"yaml-cpp/build/libyaml-cpp.a \
+-I ./code -I ../../ -I ../../yamlparserlibraries -o libyaml_parser_test -g -fsanitize=address,fuzzer
