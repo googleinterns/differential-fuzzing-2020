907bcacd8e484afbc88313945c39a9ab43bae182
pgmoka <pgmoka@live.com>
2020-09-04 02:06:45 +0000

    Makes sure comparison is correctly done

--- yaml_fuzzing/code/yaml_fuzzer.cpp
+++ yaml_fuzzing/code/yaml_fuzzer.cpp
@@ -9,7 +9,10 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t size)
         (differential_parser::Parser*)(libyaml_differential_parser::LibyamlParser::GetStaticInstance()), 
         (differential_parser::Parser*)(yamlcpp_differential_parser::YamlCppParser ::GetStaticInstance())};
 
-    differential_fuzzer::fuzzer::DifferentiallyFuzz(array_of_parsers, 2, Data, size);
+    bool fuzzers_are_different = 
+        differential_fuzzer::fuzzer::DifferentiallyFuzz(array_of_parsers, 2, Data, size);
+
+    assert(fuzzers_are_different);
 
     return 0;
 }
